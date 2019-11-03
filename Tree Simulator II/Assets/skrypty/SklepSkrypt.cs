45ec77a6509630dd257e3245372ed5d2eecb5e23
Bartosz Bień <bartekb340@gmail.com>
2019-11-03 22:22:00 +0100

    sklep repaired II

---
+++ Tree Simulator II/Assets/skrypty/SklepSkrypt.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SklepSkrypt : MonoBehaviour
+{
+    public static bool czySklepOtwarty = false;
+    public GameObject sklepUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (czySklepOtwarty)
+            {
+                nastepnyMiesiac();
+            }
+            else
+            {
+                otworzSklep();
+            }
+        }
+    }
+
+    public void otworzSklep()
+    {
+        // sie wyswietla canvas jak wywolana funkcja
+        sklepUI.SetActive(true);
+        czySklepOtwarty = true;
+    }
+
+    public void nastepnyMiesiac()
+    {
+        sklepUI.SetActive(false);
+        czySklepOtwarty = false;
+    }
+
+    public void kupDrzewo()
+    {
+        
+    }
+}
